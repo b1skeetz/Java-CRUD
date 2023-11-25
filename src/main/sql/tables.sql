9ba8d65e8096d41cd819a7fcabdc7a02bd7fdcf5
Damir Kadyrzhanov <damirk120404@gmail.com>
2023-11-25 17:07:01 +0600

    vehicle table haven't been filled

--- src/main/sql/tables.sql
+++ src/main/sql/tables.sql
@@ -1,9 +1,12 @@
+drop table Users;
+
 create table Users(
     id serial8 primary key,
     name varchar(20) not null,
     age int2 not null
 );
 
+drop table Vehicle;
 create table Vehicle(
     id serial8 primary key,
     user_id int8 not null,
