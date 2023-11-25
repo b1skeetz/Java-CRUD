454b1a58773d57ad0f97a9910e9919666c0d8af8
Damir Kadyrzhanov <damirk120404@gmail.com>
2023-11-25 17:08:42 +0600

    new Util file added

--- src/main/java/utils/EntityManagerFactoryUtil.java
+++ src/main/java/utils/EntityManagerFactoryUtil.java
@@ -1,2 +1,17 @@
-package utils;public class EntityManagerFactoryUtil {
+package utils;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerFactoryUtil {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManagerFactoryUtil(){
+        // constructor
+    }
+    public static EntityManagerFactory getEntityManagerFactory(){
+        if(entityManagerFactory == null){
+            entityManagerFactory = Persistence.createEntityManagerFactory("main");
+        }
+        return entityManagerFactory;
+    }
 }
